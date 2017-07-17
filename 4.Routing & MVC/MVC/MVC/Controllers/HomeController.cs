97fa4edb4ac15750d82a39f2930fc5a152c1550b
AndreiSpatariu <andrei_spatariu@yahoo.com>
2017-07-17 20:35:00 +0300

    AC-4 Add View Add controller Now controller returns view

---
+++ 4.Routing & MVC/MVC/MVC/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetView(ProfileData profileData)
+        {
+            return View("~/Views/Index.html", profileData);
+        }
+    }
+}
---
+++ 4.Routing & MVC/MVC/MVC/Models/ProfileData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC.Models
+{
+    public class ProfileData
+    {
+        public string PictureUrl { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return FirstName + LastName;
+            }
+            private set { }
+        }
+        public string Description { get; set; }
+        public string Hobbies { get; set; }
+    }
+}
